a82081d88cc6b4c382bb3bab7b46dac9b0baa401
Denis <carriere.denis@gmail.com>
2019-09-09 07:11:26 +0800

    fix test

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 
 # Create accounts
-cleos create account eosio myidentity EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+cleos create account eosio myprovider EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos create account eosio myaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos create account eosio identity EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 
