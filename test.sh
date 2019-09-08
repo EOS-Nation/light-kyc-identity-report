e7e35af1158d6829d2aa058b16be57dcfe739311
Denis <carriere.denis@gmail.com>
2019-09-09 06:33:26 +0800

    add error test cases

--- test.sh
+++ test.sh
@@ -10,3 +10,10 @@ cleos push action identity tier '[1, "standard", "Standard Tier for light KYC"]'
 cleos push action identity add '["myprovider", "myaccount", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", 1, "remove me"]' -p myprovider
 cleos push action identity remove '["myprovider", "myaccount"]' -p myprovider
 cleos push action identity add '["myprovider", "myaccount", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", 1, "Additional Metadata for Light KYC"]' -p myprovider
+
+# errors
+cleos push action identity add '["myaccount", "myaccount", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", 1, "provider is not authorized"]' -p myaccount
+cleos push action identity tier '[1, "standard", "missing authority of identity"]' -p myaccount
+cleos push action identity provider '["myaccount", "missing authority of identity"]' -p myaccount
+cleos push action identity provider '["myaccount", "missing authority of identity"]' -p myprovider
+cleos push action identity add '["myprovider", "myaccount", "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", 9, "tier does not exist"]' -p myprovider
